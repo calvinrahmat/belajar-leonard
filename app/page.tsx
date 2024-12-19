75cc8658d9fec0b054598cd5b7375131533d0649
Tanjiboo96 <leonardprakoso@gmail.com>
2024-12-19 16:04:31 +0700

    updated by Leon

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     
     <div>
-      <h1 className="flex justify-center text-[150px]">ShadcnUI sampe JAGO</h1>
+      <h1 className="flex justify-center text-[100px]">ShadcnUI sampe JAGO</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-4xl mx-auto">
         <div className="flex items-center justify-center space-x-4">
         <Calendar
