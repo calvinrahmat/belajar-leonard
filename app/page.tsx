722c09e75e160a3f4bb56ba06ffadaf9a10e043f
Tanjiboo96 <leonardprakoso@gmail.com>
2024-12-19 14:23:39 +0700

    updated by Leon

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,13 @@ import {
 } from "@/components/ui/card"
 import { Calendar } from "@/components/ui/calendar"
 import cardData from "./data/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function Home() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
@@ -59,4 +66,26 @@ export default function Home() {
       </div>
     </div>
   )      
+}
+
+export function CarouselDemo() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
 }
