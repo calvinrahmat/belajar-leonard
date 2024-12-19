f5fb262af934703a018cdcc277fee65389e3f2a7
Tanjiboo96 <leonardprakoso@gmail.com>
2024-12-19 15:49:07 +0700

    updated by Leon

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     
     <div>
-      <h1 className="flex justify-center text-[100px]">ShadcnUI</h1>
+      <h1 className="flex justify-center text-[150px]">ShadcnUI</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-4xl mx-auto">
         <div className="flex items-center justify-center space-x-4">
         <Calendar
@@ -58,6 +58,7 @@ export default function Home() {
               </CardContent>
               <CardFooter>
                 <p>{cardFooter}</p>
+                <Button>Photo</Button>
               </CardFooter>
             </Card>
             )
