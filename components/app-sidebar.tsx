9fc761e344fcfc618409acd769a3f87cfdef2db2
Tanjiboo96 <leonardprakoso@gmail.com>
2024-12-21 20:01:58 +0700

    updated by Leon

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -42,7 +42,7 @@ const items = [
 
 export function AppSidebar() {
   return (
-    <Sidebar side= "right">
+     <Sidebar collapsible= "offcanvas">
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
